f438506fe05cf95de68cefd44d6f18851a3d7981
Dante Wang <dante.wang@liferay.com>
2026-01-21 17:02:56 +0800

    LPD-Jakarta Move to an already exported package

--- bridge-ext/src/main/java/com/liferay/faces/bridge/ext/internal/BridgeFacesContextFactoryImpl.java
+++ bridge-ext/src/main/java/com/liferay/faces/bridge/ext/internal/BridgeFacesContextFactoryImpl.java
@@ -11,7 +11,7 @@
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
  */
-package com.liferay.faces.bridge.ext.context.internal;
+package com.liferay.faces.bridge.ext.internal;
 
 import com.liferay.faces.bridge.context.BridgeFacesContextFactory;
 import com.liferay.portal.kernel.util.PortalUtil;
